e35a4b0544e99e6d61887ce873791d8c96ac3ab1
Kapie77 <usergames02@proton.me>
2026-04-29 20:00:55 -0300

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -104,6 +104,7 @@ const translations = {
       pickaxedesc: "Esta ferramenta de mineração possui estatísticas bem equilibradas e é particularmente útil para escavar cérebros de Deadites. Contundente. Uma mão.",
       meathammerdesc: "Tem dano e velocidade medianos, mas desencadeia ataques especiais com toda a força. Contundente. Uma mão.",
       meatcleaverdesc: "O que falta a esta arma em alcance, ela compensa com velocidade e capacidade de desmembramento. Cortante. Uma mão.",
+      handaxedesc: "Esta arma oferece um equilíbrio entre dano, velocidade de ataque e desmembramento. Cortante. Uma mão.",
 
       /* Ranged Desc */
       crossbowdesc: "Essa arma extremamente poderosa é dolorosamente lenta para recarregar, então faça com que cada tiro valha a pena.",
@@ -283,6 +284,7 @@ const translations = {
       pickaxedesc: "This mining tool has well-balanced stats and is particularly useful for excavating Deadite brains. Blunt. One-handed.",
       meathammerdesc: "It's got average damage and speed, but triggers special attacks like a boss. Blunt. One-handed.",
       meatcleaverdesc: "What this weapon lacks in range, it makes up for with speed and dismemberment ability. Sharp. One-handed.",
+      handaxedesc: "This weapon balances damage, attack speed and dismemberment. Sharp. One-handed.",
 
       /* Ranged Desc */
       crossbowdesc: "This very powerful weapon is painfuly slow to reload, so make every shot count.",
