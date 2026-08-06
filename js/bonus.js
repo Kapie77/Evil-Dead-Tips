df8427f6a7c856060f686b287a4a0c061fe7a0b2
Kapie77 <usergames02@proton.me>
2026-08-06 20:04:37 -0300

    Add files via upload

---
+++ js/bonus.js
@@ -0,0 +1,488 @@
+const bonuses = [
+
+// ----------------------------------------------------------- //
+    {
+
+    image:"../images/HUD/HUD_Skills/Arrow_Stroke.png",
+    lang: "bonus1"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/DN015_Token.png",
+    lang: "bonus2"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/DN016_Token.png",
+    lang: "bonus3"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/DPB008_ViciousFatigue_Token.png",
+    lang: "bonus4"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/DW005_Token.png",
+    lang: "bonus5"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/DW011_Token.png",
+    lang: "bonus6"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/DW012_Token.png",
+    lang: "bonus7"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/Eye_Token.png",
+    lang: "bonus8"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/HUD_SH009_01_2d.png",
+    lang: "bonus9"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/HUD_SH010_01_2d.png",
+    lang: "bonus10"
+
+    },
+
+    /* 20 */
+    {
+
+    image:"../images/HUD/HUD_Skills/Progress_Bar_xxs.png",
+    lang: "bonus11"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SC033_Token.png",
+    lang: "bonus12"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SC045_Token.png",
+    lang: "bonus13"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SC046_Token.png",
+    lang: "bonus14"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SC047_Token.png",
+    lang: "bonus15"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SH012_Token.png",
+    lang: "bonus16"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL003_Token.png",
+    lang: "bonus17"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL005_Token.png",
+    lang: "bonus18"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL013_00.png",
+    lang: "bonus19"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL013_01.png",
+    lang: "bonus20"
+
+    },
+
+    /* 30 */
+    {
+
+    image:"../images/HUD/HUD_Skills/SL013_02.png",
+    lang:"bonus21"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL013_03.png",
+    lang: "bonus22"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL013_Self.png",
+    lang: "bonus23"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL015_Melee.png",
+    lang: "bonus24"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL015_Ranged.png",
+    lang: "bonus25"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL017_Hunter_Token.png",
+    lang: "bonus26"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL017_Leader_Token.png",
+    lang: "bonus27"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL017_Support_Token.png",
+    lang: "bonus28"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL017_Warrior_Token.png",
+    lang: "bonus29"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SL018_no_Token.png",
+    lang: "bonus30"
+
+    },
+
+    /* 40 */
+    {
+
+    image:"../images/HUD/HUD_Skills/SL018_yes_Token.png",
+    lang: "bonus31"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/Specialist_Token.png",
+    lang: "bonus32"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SS009_Token.png",
+    lang: "bonus33"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SS015_Token.png",
+    lang: "bonus34"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SS016_Token.png",
+    lang: "bonus35"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SS019_Token.png",
+    lang: "bonus36"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SW009_Token.png",
+    lang: "bonus37"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SW013_Token.png",
+    lang: "bonus38"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/SW014_Token.png",
+    lang: "bonus39"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1086_u0001_HUD_DP020_01_2d.png",
+    lang: "bonus40"
+
+    },
+
+    /* 50 */
+    {
+
+    image:"../images/HUD/HUD_Skills/u1087_u0001_HUD_DN019_01_2d.png",
+    lang: "bonus41"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1088_u0001_HUD_DN017_01_2d.png",
+    lang: "bonus42"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1092_u0001_HUD_SH006_01_2d.png",
+    lang: "bonus43"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1093_u0001_HUD_SH008_01_2d.png",
+    lang: "bonus44"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1095_u0001_HUD_SC034_01_2d.png",
+    lang: "bonus45"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1096_u0001_HUD_SC035_01_2d.png",
+    lang: "bonus46"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1098_u0001_HUD_SL002_01_2d.png",
+    lang: "bonus47"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1099_u0001_HUD_SL006_01_2d.png",
+    lang: "bonus48"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1100_u0001_HUD_SL007_01_2d.png",
+    lang: "bonus49"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1101_u0001_HUD_SL008_01_2d.png",
+    lang: "bonus50"
+
+    },
+
+    /* 59 */
+    {
+
+    image:"../images/HUD/HUD_Skills/u1102_u0001_HUD_SL009_01_2d.png",
+    lang: "bonus51"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1103_u0001_HUD_SL010_01_2d.png",
+    lang: "bonus52"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1104_u0001_HUD_SL012_01_2d.png",
+    lang: "bonus53"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1105_u0001_HUD_SW004_01_2d.png",
+    lang: "bonus54"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1106_u0001_HUD_SW012_01_2d.png",
+    lang: "bonus55"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1107_u0001_HUD_SH014_01_2d.png",
+    lang: "bonus56"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1108_u0001_HUD_SH016_01_2d.png",
+    lang: "bonus57"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u1109_u0001_HUD_SS002_01_2d.png",
+    lang: "bonus58"
+
+    },
+
+    {
+
+    image:"../images/HUD/HUD_Skills/u2445_u0001_Glow_01_2d.png",
+    lang: "bonus59"
+
+    }
+
+  ];
+// -------------------------------------------------------- //
+const currentLang = localStorage.getItem("lang") || "en";
+
+const grid = document.getElementById("bonusGrid");
+
+bonuses.forEach(bonus => {
+
+    const card = document.createElement("div");
+
+    card.className = "bonus-card";
+
+    card.innerHTML = `
+        <img src="${bonus.image}">
+        <p>${translations[currentLang][bonus.lang]}</p>
+    `;
+
+    card.onclick = () => openModal(bonus);
+
+    grid.appendChild(card);
+
+});
+
+const modal=document.createElement("div");
+
+modal.className="modal";
+
+modal.innerHTML=`
+
+<div class="modal-content">
+
+<img id="modalImage">
+
+<p id="modalDescription"></p>
+
+<button class="close">Fechar</button>
+
+</div>
+
+`;
+
+document.body.appendChild(modal);
+
+modal.querySelector(".close").onclick=()=>{
+
+modal.classList.remove("active");
+
+};
+
+modal.onclick=e=>{
+
+if(e.target===modal)
+
+modal.classList.remove("active");
+
+};
+
+function openModal(bonus){
+
+document.getElementById("modalImage").src=bonus.image;
+
+document.getElementById("modalDescription").textContent =
+    translations[currentLang][bonus.lang];
+
+modal.classList.add("active");
+
+}
